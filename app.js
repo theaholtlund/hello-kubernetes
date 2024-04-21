bd5844c9e15db572a89f85c914cadd7f36fe532e
theaholtlund <117310317+theaholtlund@users.noreply.github.com>
2024-04-21 09:03:49 +0200

    Update app to include public directory

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 // Import required libraries
 const express = require("express");
+const path = require("path");
 
 // Create Express instance, and define port
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, "public")));
+
 // Define route handler for default home page
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 // Define route handler to get current date and time
