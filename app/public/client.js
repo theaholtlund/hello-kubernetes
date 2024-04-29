b4009c4cba356c123bb8b2db39c1f8ac8da87c44
theaholtlund <117310317+theaholtlund@users.noreply.github.com>
2024-04-29 11:55:32 +0200

    Add back date and time after refactoring code

--- app/public/client.js
+++ app/public/client.js
@@ -100,12 +100,5 @@ function deleteGreeting(greetingId) {
     });
 }
 
-// Fetch current date and time from the server and update the DOM
-fetch("/currentdatetime")
-  .then((response) => response.json())
-  .then((data) => {
-    document.getElementById("currentDateTime").textContent = data.datetime;
-  });
-
 // Initial fetch and display of greetings on page load
 fetchAndDisplayGreetings();
