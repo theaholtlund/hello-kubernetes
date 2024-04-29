adbce5ec672fbf3ca40db878539ad2a2bb7f5534
theaholtlund <117310317+theaholtlund@users.noreply.github.com>
2024-04-29 11:40:10 +0200

    Move client-side functionality for datetime

--- app/public/client.js
+++ app/public/client.js
@@ -100,5 +100,12 @@ function deleteGreeting(greetingId) {
     });
 }
 
+// Fetch current date and time from the server and update the DOM
+fetch("/currentdatetime")
+  .then((response) => response.json())
+  .then((data) => {
+    document.getElementById("currentDateTime").textContent = data.datetime;
+  });
+
 // Initial fetch and display of greetings on page load
 fetchAndDisplayGreetings();
--- app/public/datetime.js
+++
@@ -1,6 +0,0 @@
-// Fetch current date and time from the server and update the DOM
-fetch("/currentdatetime")
-  .then((response) => response.json())
-  .then((data) => {
-    document.getElementById("currentDateTime").textContent = data.datetime;
-  });
